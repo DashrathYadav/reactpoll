0c368bf666a35faac4650f642bd26d477ee89163
vishal kumar <vishalkumar437@gmail.com>
2023-07-12 21:29:35 +0530

    Adding countsupport

--- src/Components/poll/PollingUi.jsx
+++ src/Components/poll/PollingUi.jsx
@@ -1,110 +1,150 @@
-import React, { useState } from "react";
-import './PollingUi.css'
-import { useSelector } from "react-redux"
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import "./PollingUi.css";
+import { useSelector } from "react-redux";
+import axios from "axios";
+
 export default function PollingUi() {
 
-  {/* Need to fetch PollId and information will have to create get for poll to get the details */}
-  const category = ["a","b"];
-  const question = "This is a Question";
-  const creatorId = "dummyCreatorId";
-  const totalVotes = "20";
-  const pollid = "dummyPollId"
+  const pid = { pid: "64aec382d229c8f9872c3df5" };
 
-  const [formData, setformData] = useState({
-   
+  
+  const [fetchPoll, setFetchPoll] = useState({
+    category: [],
+    question: "",
+    creatorId: "",
+    totalVotes: "",
+    pollid: "",
+    options: [],
   });
 
+  const getData = () => {
+    axios
+      .post("http://localhost:3000/getPoll", pid, {
+        headers: { "Access-Control-Allow-Origin": "*" },
+      })
+      .then((response) => {
+        const got = response.data.poll;
+
+        setFetchPoll({
+          category: got.Category,
+          question: got.Question,
+          creatorId: got.creatorId,
+          totalVotes: got.voter_ids.length,
+          pollid: got._id,
+          options: got.Options.map(option => ({
+            option: got.opt,
+            count: got.count
+          }))
+        });
+        console.log(fetchPoll.category)
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+ 
 
   let id = useSelector((state) => {
     return state.component.loginId;
   });
 
-    const handlePolling= (event)=>{
-      console.log(selectedItem)
-      event.preventDefault();
-      const data = {
-        pollId : pollid,
-        voter_Id : id,
-        option : selectedItem
-      }
-      console.log(data)
-      axios
-      .post("http://localhost:3000/", data, {
+  const handlePolling = (event) => {
+    console.log(selectedItem);
+    event.preventDefault();
+    const data = {
+      pollId: fetchPoll.pollid,
+      voter_Id: id,
+      option: selectedItem,
+    };
+    console.log(data);
+    axios
+      .post("http://localhost:3000/submitPoll", data, {
         headers: { "Access-Control-Allow-Origin": "*" },
       })
       .then((response) => {
         console.log(response.status);
-        }
-      )
+      })
       .catch((error) => {
         console.log(error.message);
       });
-    }
+  };
 
-    const submitBtn = (
-      <a
-        className="btn SignUp--lessMargine"
-        onMouseOver={(e) => {
-          const btnEl = e.target;
-          e.target.addEventListener("mouseover", (event) => {
-            const x = event.pageX - btnEl.offsetLeft;
-            const y = event.pageY - btnEl.offsetTop;
-  
-            btnEl.style.setProperty("--xPos", x + "px");
-            btnEl.style.setProperty("--yPos", y + "px");
-          });
-        }}
-        onClick={handlePolling}
-      >
-        <span>Submit</span>
-      </a>
-    );
+  const submitBtn = (
+    <a
+      className="btn SignUp--lessMargine"
+      onMouseOver={(e) => {
+        const btnEl = e.target;
+        e.target.addEventListener("mouseover", (event) => {
+          const x = event.pageX - btnEl.offsetLeft;
+          const y = event.pageY - btnEl.offsetTop;
 
-    const [selectedItem,setSelectedItem] = useState(null);
-    const options = [{text:"1.hello",coun:0},{text:"2.bye",count:0}]
-    
-    const handleItemClick = (index)=>{
-      setSelectedItem(index);
-    }
+          btnEl.style.setProperty("--xPos", x + "px");
+          btnEl.style.setProperty("--yPos", y + "px");
+        });
+      }}
+      onClick={handlePolling}
+    >
+      <span>Submit</span>
+    </a>
+  );
+
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleItemClick = (index) => {
+    setSelectedItem(index);
+  };
 
   return (
     <div>
       <div className="pollingUi--infoContainer">
         <p className="pollingUi--questionpollingUi--flex" id="Question">
           <span className="PollUi--span">Question : </span>
-          <span className="pollingUi--span-ans"> {question}</span>
+          <span className="pollingUi--span-ans"> {fetchPoll.question}</span>
         </p>
         <p className="pollingUi--CategorypollingUi--flex" id="category">
           <span className="pollingUi--span">category :</span>
           <span className="pollingUi--span-ans">
             {" "}
-            {category?.map((ele) => {return ele + "  ";})}
+            {fetchPoll.category?.map((ele) => {
+              return ele + "  ";
+            })}
           </span>
         </p>
         <p className="pollingUi--totvotespollingUi--flex">
           <span className="pollingUi--span">Total-Votes :</span>{" "}
-          <span className="pollingUi--span-ans"> {totalVotes}</span>
+          <span className="pollingUi--span-ans"> {fetchPoll.totalVotes}</span>
         </p>
         <p className="pollingUi--creatorpollingUi--flex">
           <span className="pollingUi--span">Creator ID :</span>{" "}
-          <span className="pollingUi--span-ans"> {creatorId}</span>
+          <span className="pollingUi--span-ans"> {fetchPoll.creatorId}</span>
         </p>
-
       </div>
 
-      <div className="pollingUi--optionContainer"> <span className="pollingUi--span">Option :</span>
-      
-            {options.map((option,index) => {
-              return (
-              <div key={index} className="pollingUi--insideOptionContainer">
-                <input type="radio" id = {`radio-${index}`} name ="radio-group" checked ={selectedItem === index} onChange={()=>handleItemClick(index)} value={index}/>
-                <label htmlFor={`radio-${index}`}>{option.text}</label>
-              </div>
-            ) })}
-          </div>
-          {/* {submitBtn} */}
-          <button onClick={handlePolling}>Submit</button>
+      <div className="pollingUi--optionContainer">
+        {" "}
+        <span className="pollingUi--span">Option :</span>
+        {fetchPoll.options.map((option, index) => {
+          return (
+            <div key={index} className="pollingUi--insideOptionContainer">
+              <input
+                type="radio"
+                id={`radio-${index}`}
+                name="radio-group"
+                checked={selectedItem === index}
+                onChange={() => handleItemClick(index)}
+                value={index}
+              />
+              <label htmlFor={`radio-${index}`}>{fetchPoll.options.text}</label>
+            </div>
+          );
+        })}
+      </div>
+      {/* {submitBtn} */}
+      <button onClick={handlePolling}>Submit</button>
     </div>
   );
 }
